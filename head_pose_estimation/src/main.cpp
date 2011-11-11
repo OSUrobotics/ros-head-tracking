06651e5363b8a08d8b57ff46fce49d66245257f4
Dan Lazewatsky <dan@lazewatsky.com>
2011-11-11 21:05:05 +0000

    have to convert to mm for this to work

--- head_pose_estimation/src/main.cpp
+++ head_pose_estimation/src/main.cpp
@@ -59,7 +59,7 @@
 #include <ros/ros.h>
 #include "sensor_msgs/Image.h"
 #include "sensor_msgs/PointCloud2.h"
-// #include "image_transport/image_transport.h"
+#include "image_transport/image_transport.h"
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
@@ -104,43 +104,91 @@ Mat g_im3D;
 
 CRForestEstimator estimator;
 
+
+
 std::vector< cv::Vec<float,POSE_SIZE> > g_means; //outputs
 std::vector< std::vector< Vote > > g_clusters; //full clusters of votes
 std::vector< Vote > g_votes; //all votes returned by the forest
 
 void loadConfig(ros::NodeHandle nh) {
-	nh.param("tree_path",			g_treepath, string(""));
-	nh.param("ntrees",				g_ntrees, 10);
-	nh.param("max_variance",		g_maxv, 800.0);
-	nh.param("larger_radius_ratio",	g_larger_radius_ratio, 1.0);
-	nh.param("smaller_radius_ratio",g_smaller_radius_ratio, 6.0);
-	nh.param("stride",				g_stride, 5);
-	nh.param("head_threshold",		g_th, 400);
+	nh.param("tree_path",			g_treepath,				string("trees/tree"));
+	nh.param("ntrees",				g_ntrees,				10);
+	nh.param("max_variance",		g_maxv,					800.0);
+	nh.param("larger_radius_ratio",	g_larger_radius_ratio,	1.0);
+	nh.param("smaller_radius_ratio",g_smaller_radius_ratio,	6.0);
+	nh.param("stride",				g_stride,				5);
+	nh.param("head_threshold",		g_th,					400);
+	
+	ROS_INFO("------------------------------------");
+	ROS_INFO("Trees:            %d", g_ntrees);
+	ROS_INFO("Stride:           %d", g_stride);
+	ROS_INFO("Max Variance:     %f", g_maxv);
+	ROS_INFO("Head Threshold:   %d", g_th);
+	ROS_INFO("------------------------------------");
+	
 }
 
 void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
+//void depthCallback(const sensor_msgs::ImageConstPtr& depth_msg, const sensor_msgs::CameraInfoConstPtr& info)
 {
+
 	PointCloud cloud;
 	pcl::fromROSMsg(*msg, cloud);
-	Mat img3D;
-	img3D.create( cloud.height, cloud.width, CV_32FC3 );
-	
+//	cv_bridge::CvImagePtr cv_ptr;
+//	try{
+//		cv_ptr = cv_bridge::toCvCopy(depth_msg, sensor_msgs::image_encodings::TYPE_32FC1);
+//	}catch (cv_bridge::Exception& e){
+//		ROS_ERROR("cv_bridge exception: %s", e.what());
+//	}
 
+//	cv::Mat depthImg = cv_ptr->image;
+	Mat img3D;
+	img3D.create(cloud.height, cloud.width, CV_32FC3);
+	//img3D.create( depthImg.rows, depthImg.cols, CV_32FC3 );
+	
 	//get 3D from depth
 	for(int y = 0; y < img3D.rows; y++) {
 		Vec3f* img3Di = img3D.ptr<Vec3f>(y);
 	
 		for(int x = 0; x < img3D.cols; x++) {
-			img3Di[x][0] = cloud.at(x, y).x;
-			img3Di[x][1] = cloud.at(x, y).y;
-			img3Di[x][2] = cloud.at(x, y).z;
+			if(cloud.at(x,y).z < 2) { //this part is a bit of a hack - eventaully, do real head segmentation
+				img3Di[x][0] = cloud.at(x, y).x*1000;
+				img3Di[x][1] = cloud.at(x, y).y*1000;
+				img3Di[x][2] = cloud.at(x, y).z*1000;
+}
 		}
 	}
+
+//	for(int y = 0; y < img3D.rows; y++)
+//	{
+//		Vec3f* img3Di = img3D.ptr<Vec3f>(y);
+//		const float* depthImgi = depthImg.ptr<float>(y);
+//	
+//		for(int x = 0; x < img3D.cols; x++){
+//	
+//			float d = depthImgi[x] * 1000;
+//			if ( d < 2000 ){
+//				img3Di[x][0] = d * (float(x) - info->K[2])/info->K[0];
+//				img3Di[x][1] = d * (float(y) - info->K[5])/info->K[4];
+//				img3Di[x][2] = d;
+//				
+//	
+//			}
+//			else{
+//				img3Di[x] = 0;
+//			}
+//	
+//		}
+//	}
 	
+	//exit(0);
+	
+	imshow("win", img3D);
+	waitKey(100);
 	g_means.clear();
 	g_votes.clear();
 	g_clusters.clear();
-
+	
 	//do the actual estimate
 	estimator.estimate( 	img3D,
 							g_means,
@@ -165,8 +213,8 @@ void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
 		float pt2d_est[2];
 		float pt2d_gt[2];
 		
-		// pt2d_est[0] = depth_intrinsic[0]*g_means[0][0]/g_means[0][2] + depth_intrinsic[2];
-		// pt2d_est[1] = depth_intrinsic[4]*g_means[0][1]/g_means[0][2] + depth_intrinsic[5];
+		// pt2d_est[0] = info->K[0]*g_means[0][0]/g_means[0][2] + info->K[2];
+		// pt2d_est[1] = info->K[4]*g_means[0][1]/g_means[0][2] + info->K[5];
 	
 	}
 	
@@ -174,13 +222,15 @@ void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
 
 int main(int argc, char* argv[])
 {
-
+	namedWindow("win");
 	ros::init(argc, argv, "head_pose_estimator");
 	ros::NodeHandle nh;
+	image_transport::ImageTransport it(nh);
 	ros::Subscriber cloud_sub = nh.subscribe<sensor_msgs::PointCloud2>("cloud", 1, cloudCallback);
+	//image_transport::CameraSubscriber depth_sub = it.subscribeCamera("depth", 1, depthCallback);
 	
 	loadConfig(nh);
-	
+	ROS_INFO("tree path: %s", g_treepath.c_str());
 	if( !estimator.loadForest(g_treepath.c_str(), g_ntrees) ){
 		ROS_ERROR("could not read forest!");
 		exit(-1);
