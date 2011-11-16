559809b51fa7b1e22361ffe23cb88fbb7e931cc3
Dan Lazewatsky <dan@lazewatsky.com>
2011-11-16 20:02:40 +0000

    more code cleanup fixed parameters

--- head_pose_estimation/src/main.cpp
+++ head_pose_estimation/src/main.cpp
@@ -121,14 +121,24 @@ std::vector< Vote > g_votes; //all votes returned by the forest
 cv::Rect roi;
 bool roiReady = false;
 
-void loadConfig(ros::NodeHandle nh) {
+void loadConfig() {
+	ros::NodeHandle nh("~");
 	nh.param("tree_path",			g_treepath,				string("trees/tree"));
 	nh.param("ntrees",				g_ntrees,				10);
 	nh.param("max_variance",		g_maxv,					800.0);
 	nh.param("larger_radius_ratio",	g_larger_radius_ratio,	1.0);
 	nh.param("smaller_radius_ratio",g_smaller_radius_ratio,	6.0);
 	nh.param("stride",				g_stride,				5);
-	nh.param("head_threshold",		g_th,					400);	
+	nh.param("head_threshold",		g_th,					400);
+	
+	// 
+	// ROS_INFO("tree_path			%s",g_treepath.c_str());
+	// ROS_INFO("ntrees				%d",g_ntrees);
+	// ROS_INFO("max_variance		%f",g_maxv);
+	// ROS_INFO("larger_radius_ratio	%f",g_larger_radius_ratio);
+	// ROS_INFO("smaller_radius_ratio%f",g_smaller_radius_ratio);
+	// ROS_INFO("stride				%d",g_stride);
+	// ROS_INFO("head_threshold		%d",g_th);
 }
 
 void roiCallback(const sensor_msgs::RegionOfInterest::ConstPtr& msg) {
@@ -189,44 +199,46 @@ void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
 						);
 	
 	//assuming there's only one head in the image!
-	geometry_msgs::PoseStamped pose_msg;
-	pose_msg.header.stamp = ros::Time::now();
-	pose_msg.header.frame_id = msg->header.frame_id;
+	if(g_means.size() > 0) {	
+		geometry_msgs::PoseStamped pose_msg;
+		pose_msg.header.stamp = ros::Time::now();
+		pose_msg.header.frame_id = msg->header.frame_id;
 	
-	cv::Vec<float,POSE_SIZE> pose(g_means[0]);
+		cv::Vec<float,POSE_SIZE> pose(g_means[0]);
 	
-	KDL::Rotation r = KDL::Rotation::RPY(
-										 from_degrees( pose[5]    ), 
-										 from_degrees(-pose[3]+180),
-										 from_degrees(-pose[4]    )
-										);
-	double qx, qy, qz, qw;
-	r.GetQuaternion(qx, qy, qz, qw);
+		KDL::Rotation r = KDL::Rotation::RPY(
+											 from_degrees( pose[5]    ), 
+											 from_degrees(-pose[3]+180),
+											 from_degrees(-pose[4]    )
+											);
+		double qx, qy, qz, qw;
+		r.GetQuaternion(qx, qy, qz, qw);
 	
-	geometry_msgs::PointStamped head_point;
-	geometry_msgs::PointStamped head_point_transformed;
+		geometry_msgs::PointStamped head_point;
+		geometry_msgs::PointStamped head_point_transformed;
 	
-	head_point.header = pose_msg.header;
+		head_point.header = pose_msg.header;
 
-	head_point.point.x = pose[0]/1000;
-	head_point.point.y = pose[1]/1000;
-	head_point.point.z = pose[2]/1000;
+		head_point.point.x = pose[0]/1000;
+		head_point.point.y = pose[1]/1000;
+		head_point.point.z = pose[2]/1000;
 	
-	try {
-		listener->transformPoint("/openni_depth_frame", head_point, head_point_transformed);
-	} catch(tf::TransformException ex) {
-		ROS_WARN("Transform exception, dropping pose (don't worry, this is probably ok)");
-		return;
+		try {
+			listener->transformPoint("/openni_depth_frame", head_point, head_point_transformed);
+		} catch(tf::TransformException ex) {
+			ROS_WARN("Transform exception, dropping pose (don't worry, this is probably ok)");
+			return;
+		}
+		pose_msg.pose.position = head_point_transformed.point;
+		pose_msg.header.frame_id = head_point_transformed.header.frame_id;
+	
+		pose_msg.pose.orientation.x = qx;
+		pose_msg.pose.orientation.y = qy;
+		pose_msg.pose.orientation.z = qz;
+		pose_msg.pose.orientation.w = qw;
+
+		pose_pub.publish(pose_msg);
 	}
-	pose_msg.pose.position = head_point_transformed.point;
-	pose_msg.header.frame_id = head_point_transformed.header.frame_id;
-	
-	pose_msg.pose.orientation.x = qx;
-	pose_msg.pose.orientation.y = qy;
-	pose_msg.pose.orientation.z = qz;
-	pose_msg.pose.orientation.w = qw;
-
-	pose_pub.publish(pose_msg);
 }
 
 int main(int argc, char* argv[])
@@ -241,7 +253,7 @@ int main(int argc, char* argv[])
 	
 	pose_pub = nh.advertise<geometry_msgs::PoseStamped>("head_pose", 1);
 	
-	loadConfig(nh);
+	loadConfig();
 	ROS_INFO("tree path: %s", g_treepath.c_str());
 	if( !estimator.loadForest(g_treepath.c_str(), g_ntrees) ){
 		ROS_ERROR("could not read forest!");
