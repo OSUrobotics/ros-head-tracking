0f1034e8973b0ed3fedd78e1c4110d2c00047b89
Dan Lazewatsky <dan@lazewatsky.com>
2011-12-12 21:51:11 +0000

    moved some functions to headmath

--- head_pose_estimation/head_pose_filter.py
+++ head_pose_estimation/head_pose_filter.py
@@ -2,32 +2,14 @@
 import roslib; roslib.load_manifest('head_pose_estimation')
 import rospy
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion 
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from headmath.conversions import pose_quat_to_euler, pose_euler_to_quat
 from collections import deque
 import numpy as np
 
+
+
 poses = None
 pose_pub = None
-
-def pose_quat_to_euler(pose_msg):
-	return np.concatenate([
-		[
-			pose_msg.pose.position.x,
-			pose_msg.pose.position.y,
-			pose_msg.pose.position.z
-		],
-		euler_from_quaternion([
-			pose_msg.pose.orientation.x,
-			pose_msg.pose.orientation.y,
-			pose_msg.pose.orientation.z,
-			pose_msg.pose.orientation.w
-	    ])])
-	
-def pose_euler_to_quat(pose):
-	return Pose(
-		Point(*pose[:3]),
-		Quaternion(*(quaternion_from_euler(*pose[3:])))
-	  )
 
 def pose_sub(pose_msg):
 	poses.append(pose_quat_to_euler(pose_msg))
