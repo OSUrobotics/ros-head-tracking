da75c0c1a9fa29f99089cf839f15dc2a957e6499
Dan Lazewatsky <dan@lazewatsky.com>
2012-02-29 19:20:54 +0000

    Art demo

---
+++ rosmouse/art_demo.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('rosmouse')
+import pygame
+from pygame.locals import * 
+from math import sqrt, pi, exp, sin
+import time
+
+def gaussian(x, sigma):
+	# sigma = 15.0
+	mu = 1
+	return exp(-(x-mu)**2/(2*sigma*sigma)) / (sigma*sqrt(2*pi))
+
+class Line(object):
+	brightness = 255
+	def __init__(self, p1, p2):
+		self.p1 = p1
+		self.p2 = p2
+		self.stamp = time.clock()
+		
+	def draw(self, surface):
+		c = (int(self.brightness*(self.stamp/time.clock())**2), 0, 0)
+		pygame.draw.aaline(surface, c, self.p1, self.p2)
+
+class Gaussian(object):
+	brightness = 255
+	def __init__(self, center, sigma=15.0, decay=False):
+		self.sigma = sigma
+		self.radius = self.find_radius()
+		self.center = center
+		self.stamp = time.clock()
+		
+	def find_radius(self):
+		c_max = self.find_max()
+		c = int(self.brightness*gaussian(1, self.sigma)/c_max)
+		r = 2
+		while c > 0:
+			c = int(self.brightness*gaussian(r, self.sigma)/c_max)
+			r += 1
+		return r
+		
+	def find_max(self):
+		return gaussian(1, self.sigma)
+		
+	# def draw(self, screen):
+	# 	c_max = self.find_max()
+	# 	TRANSPARENT = (255,0,255)
+	# 	for r in range(self.radius, 0, -1):
+	# 		s = pygame.Surface((r,r))
+	# 		# s.fill(TRANSPARENT)
+	# 		# s.set_colorkey(TRANSPARENT)
+	# 		red = int(self.brightness*gaussian(r, self.sigma)/c_max)
+	# 		c = (self.brightness, 0, 0)
+	# 		# pygame.draw.circle(screen, pygame.Color(self.brightness, 0, 0, 255-red), self.center, r)
+	# 		# s.fill((255, 255, 255, 255-red))
+	# 		
+	# 		# pygame.draw.circle(s, (0, self.brightness, 0, 255-red), self.center, r)
+	# 		pygame.draw.circle(s, (red,0,0), (r/2,r/2), r)
+	# 		s.set_alpha(red)
+	# 		screen.blit(s, (self.center[0]-r/2, self.center[1]-r/2))
+	def draw(self, screen):
+		pygame.draw.circle(screen, (int(self.brightness*(self.stamp/time.clock())**2), 0, 0), self.center, int(self.sigma))
+			
+		
+
+class ArtDemo(object): 
+	width = 500
+	height = 500
+	pos = None
+	running = False
+	gaussians = []
+
+	def __init__(self, fullscreen=False):
+		self.screen = pygame.display.set_mode((self.width, self.height))
+		if fullscreen:
+			modes = pygame.display.list_modes()
+			pygame.display.set_mode(modes[0], FULLSCREEN)
+		
+		pygame.mouse.set_visible(False)
+
+	def process_events(self, events): 
+		for event in events: 
+			if event.type == QUIT: 
+				self.running = False
+			elif event.type == KEYUP:
+				if event.key == 27:
+					self.running = False
+			elif event.type == MOUSEMOTION:
+				self.pos = event.pos
+			else: 
+				# print event
+				pass
+			
+	def run(self):
+		self.running = True
+		last_pos = self.pos
+		while self.running:
+			self.process_events(pygame.event.get())
+			if not self.pos: continue
+			if not last_pos:
+				last_pos = self.pos
+				continue
+			self.screen.fill((0,0,0))
+			# pygame.draw.circle(self.screen, (255,0,0), (50,50), 5)
+			# self.draw_gaussian(50, self.pos)
+			# self.gaussians.append(Gaussian(self.pos))
+			# for g in self.gaussians:
+			# 	g.draw(self.screen)
+			self.gaussians.append(Line(self.pos, last_pos))
+			for g in self.gaussians:
+				g.draw(self.screen)
+			pygame.draw.circle(self.screen, (255*abs(sin(time.clock()*2)), 0, 0), self.pos, 5)
+			pygame.display.flip()
+			last_pos = self.pos
+
+
+if __name__ == '__main__':
+	d = ArtDemo(fullscreen=True)
+	d.run()
