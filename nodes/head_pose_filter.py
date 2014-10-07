afd2c251346968268289a94028185afaf3bd791b
Dan Lazewatsky <dan@lazewatsky.com>
2014-10-07 11:42:05 -0500

    git rid of other unused packages in this stack

--- nodes/head_pose_filter.py
+++ nodes/head_pose_filter.py
@@ -2,9 +2,28 @@
 import roslib; roslib.load_manifest('head_pose_estimation')
 import rospy
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion 
-from headmath.conversions import pose_quat_to_euler, pose_euler_to_quat
 from collections import deque
 import numpy as np
+
+def pose_quat_to_euler(pose_msg):
+	return np.concatenate([
+		[
+			pose_msg.pose.position.x,
+			pose_msg.pose.position.y,
+			pose_msg.pose.position.z
+		],
+		euler_from_quaternion([
+			pose_msg.pose.orientation.x,
+			pose_msg.pose.orientation.y,
+			pose_msg.pose.orientation.z,
+			pose_msg.pose.orientation.w
+		])])
+	
+def pose_euler_to_quat(pose):
+	return Pose(
+		Point(*pose[:3]),
+		Quaternion(*(quaternion_from_euler(*pose[3:])))
+	)
 
 
 
